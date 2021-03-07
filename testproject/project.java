fa5197e87c91e9f65cbea812e1a538e9b3f41b23
Christian Miranda <christian.miranda.602@gmail.com>
2021-03-07 11:56:40 -0700

    init 1.1 1156 03-07-21

---
+++ testproject/project.java
@@ -0,0 +1,11 @@
+
+public class project {
+    public static void main(String[] args
+        var java = cool;
+       if (java = false) {
+           continue;
+       } else {
+           System.out.println("Python for the win.");
+       } 
+    }
+}
